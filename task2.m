498da677e5abb80695cd2e0be56a83dd5c7bb664
Apostu Izabela-Elena <iza.elena03@gmail.com>
2023-11-10 22:29:31 +0200

    Updated files

--- task2.m
+++ task2.m
@@ -1,27 +1,3 @@
-## Copyright (C) 2021 Andrei
-##
-## This program is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-## -*- texinfo -*-
-## @deftypefn {} {@var{retval} =} task1 (@var{photo}, @var{k})
-##
-## @seealso{}
-## @end deftypefn
-
-## Author: Andrei <Andrei@DESKTOP-PK505U9>
-## Created: 2021-09-14
-
 function new_X = task1(photo, k)
   [m, n] = size(photo);
   
